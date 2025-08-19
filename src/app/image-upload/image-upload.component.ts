6812fd663ef2430bbf45f2f47c00cda1dcebcc18
ranostaj <jan.ranostaj@gmail.com>
2025-08-19 12:57:59 +0200

    camera settings

--- src/app/image-upload/image-upload.component.ts
+++ src/app/image-upload/image-upload.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { Camera, CameraResultType, CameraSource } from '@capacitor/camera';
+import { Camera, CameraResultType, CameraSource, CameraDirection } from '@capacitor/camera';
 
 @Component({
   selector: 'app-image-upload',
@@ -121,10 +121,12 @@ export class ImageUploadComponent {
     try {
       const image = await Camera.getPhoto({
         quality: 100,
-        allowEditing: false,
-        saveToGallery: false, 
+        allowEditing: true,
+        saveToGallery: false,
         resultType: CameraResultType.DataUrl,
         source: CameraSource.Camera,
+        direction: CameraDirection.Rear,
+        correctOrientation: true
       });
       
       this.imageUrl = image.dataUrl || null;
